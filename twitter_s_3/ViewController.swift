c6c745bcdc62046e9c1d6a3194d876fe1a967f16
Shandler Mason <shandlermason@Shandlers-iMac.local>
2020-05-13 18:56:41 -0400

    initial commmit

---
+++ twitter_s_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twitter_s_3
+//
+//  Created by Shandler Mason on 5/13/20.
+//  Copyright © 2020 Shandler Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
